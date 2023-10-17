e825e5d5e799e114ac98536d3cbd5a27a2c7d943
Daniel Ramsgard <139645652+DanielRamsgard@users.noreply.github.com>
2023-10-17 12:31:48 -0400

    Update Artificial_Neuron.py

--- Artificial_Neuron.py
+++ Artificial_Neuron.py
@@ -1,14 +1,10 @@
 import math
+
+
 def activate(inputs, weights):
-    sums = []
-    for i in range(0, len(inputs)):
-        current_sum = inputs[i] * weights[i]
-        sums.append(current_sum)
-    final_sum = sum(sums) # computation for h for passing through sigmoid
-    return_value = 1 / (1 + math.exp(-final_sum)) # return sigmoid(h)
-    return return_value
+    """Artificial neuron."""
+    return 1 / (1 + math.exp(-sum([inputs[i] * weights[i] for i in range(len(inputs))])))
+
+
 if __name__ == "__main__":
-    inputs = [.5, .3, .2]
-    weights = [.4, .7, .2]
-    output =  activate(inputs, weights)
-    print(output)
+    print(activate([.5, .3, .2], [.4, .7, .2]))
